86bee979db9a58ceea4f0e2f3de96ba38889e5c0
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-03-15 21:55:43 +0300

    dashboard updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,7 +35,6 @@
                 </ul>
             </div>
         @endif
-        
         <div class="tab-content">
             <div class="tab-pane tabs-animation fade show active" id="tab-content-0" role="tabpanel">
                 <div class="">
@@ -43,29 +42,41 @@
                         <div class="col-lg-4 col-xl-4">
                             <div class="card mb-3 widget-content">
                                 <div class="widget-content-wrapper">
+
                                     <div class="widget-content-left">
-                                        <div class="widget-heading">Download Excel File</div>
-                                        <div class="widget-subheading">Download the guide...</div>
+                                        <div class="widget-heading">Download Excel File
+                                            <a style="color: black;" data-toggle="tooltip" data-placement="top" title="Sometext" class="btn btn-xs">?</a>
+                                        </div>
+                                        <div class="widget-subheading">Download the guide...
+
+                                        </div>
                                     </div>
                                     <div class="widget-content-right">
                                         <div class="widget-numbers text-success">
-                                            <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download</a>
+                                            <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download
+                                                {{-- <i class="fas fa-file-excel"></i> --}}
+                                                <img height="20px" src="{{ asset('icons/excel.png')}}" alt="">
+                                            </a>
+
+                                                {{-- <img height="35px" src="{{ asset('icons/excel.png')}}" alt=""> --}}
                                         </div>
                                     </div>
+
                                 </div>
                             </div>
+
                         </div>
 
                         <div class="col-lg-6 col-xl-5">
                             <div class="card mb-3 widget-content">
                                 <div class="widget-content-wrapper">
                                     <div class="widget-content-left">
-                                        <div class="widget-heading">Upload Excel File</div>
+                                        <div class="widget-heading">Upload Excel File<a style="color: black;" data-toggle="tooltip" data-placement="top" title="Upload something following the format" class="btn btn-xs">?</a></div>
                                         <div class="widget-subheading">The csv file must be a file of type: csv, txt.</div>
                                     </div>
                                     <div class="widget-content-right">
                                         <div class="widget-numbers text-success">
-                                            <button class="btn btn-primary" data-toggle="modal" data-target="#csvImportModal">Upload</button>
+                                            <button class="btn btn-primary" data-toggle="modal" data-target="#csvImportModal">Upload <i class="fas fa-upload"></i></button>
                                         </div>
                                     </div>
                                 </div>
@@ -76,154 +87,157 @@
                 </div>
                 <div class="divider"></div>
                 <div class="row">
-                    <div class="col-lg-6 col-xl-3">
+                    <div class="col-lg-6 col-xl-4">
                         <div class="card mb-3 widget-content">
                             <div class="widget-content-wrapper">
                                 <div class="widget-content-left">
-                                    <div class="widget-heading">Sponsor template</div>
+                                    <div class="widget-heading">Sponsor template<a style="color: black;" data-toggle="tooltip" data-placement="top" title="Download something!" class="btn btn-xs">?</a></div>
                                 </div>
                                 <div class="widget-content-right">
                                     <div class="widget-numbers text-success">
-                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download</a>
+                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download <img height="20px" src="{{ asset('icons/word.png')}}" alt=""></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-6 col-xl-3">
+                    <div class="col-lg-6 col-xl-4">
                         <div class="card mb-3 widget-content">
                             <div class="widget-content-wrapper">
                                 <div class="widget-content-left">
-                                    <div class="widget-heading">Delegate template</div>
+                                    <div class="widget-heading">Delegate template<a style="color: black;" data-toggle="tooltip" data-placement="top" title="Download something!" class="btn btn-xs">?</a></div>
                                 </div>
                                 <div class="widget-content-right">
                                     <div class="widget-numbers text-success">
-                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download</a>
+                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download <img height="20px" src="{{ asset('icons/word.png')}}" alt=""></a></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-6 col-xl-3">
+                    <div class="col-lg-6 col-xl-4">
                         <div class="card mb-3 widget-content">
                             <div class="widget-content-wrapper">
                                 <div class="widget-content-left">
-                                    <div class="widget-heading">Speaker template</div>
+                                    <div class="widget-heading">Speaker template<a style="color: black;" data-toggle="tooltip" data-placement="top" title="Download something!" class="btn btn-xs">?</a></div>
                                 </div>
                                 <div class="widget-content-right">
                                     <div class="widget-numbers text-success">
-                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download</a>
+                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download <img height="20px" src="{{ asset('icons/word.png')}}" alt=""></a></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-6 col-xl-3">
+                    <div class="col-lg-6 col-xl-4">
                         <div class="card mb-3 widget-content">
                             <div class="widget-content-wrapper">
                                 <div class="widget-content-left">
-                                    <div class="widget-heading">Guest of Honor</div>
+                                    <div class="widget-heading">Guest of Honor<a style="color: black;" data-toggle="tooltip" data-placement="top" title="Download something!" class="btn btn-xs">?</a></div>
                                 </div>
                                 <div class="widget-content-right">
                                     <div class="widget-numbers text-success">
-                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-lg-6 col-xl-3">
-                        <div class="card mb-3 widget-content">
-                            <div class="widget-content-wrapper">
-                                <div class="widget-content-left">
-                                    <div class="widget-heading">Exhibitor</div>
-                                </div>
-                                <div class="widget-content-right">
-                                    <div class="widget-numbers text-success">
-                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download</a>
+                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download <img height="20px" src="{{ asset('icons/word.png')}}" alt=""></a></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-6 col-xl-3">
+                    <div class="col-lg-6 col-xl-4">
                         <div class="card mb-3 widget-content">
                             <div class="widget-content-wrapper">
                                 <div class="widget-content-left">
-                                    <div class="widget-heading">Media</div>
+                                    <div class="widget-heading">Exhibitor<a style="color: black;" data-toggle="tooltip" data-placement="top" title="Download something!" class="btn btn-xs">?</a></div>
                                 </div>
                                 <div class="widget-content-right">
                                     <div class="widget-numbers text-success">
-                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download</a>
+                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download <img height="20px" src="{{ asset('icons/word.png')}}" alt=""></a></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-6 col-xl-3">
+                    <div class="col-lg-6 col-xl-4">
                         <div class="card mb-3 widget-content">
                             <div class="widget-content-wrapper">
                                 <div class="widget-content-left">
-                                    <div class="widget-heading">Partners</div>
+                                    <div class="widget-heading">Media<a style="color: black;" data-toggle="tooltip" data-placement="top" title="Download something!" class="btn btn-xs">?</a></div>
                                 </div>
                                 <div class="widget-content-right">
                                     <div class="widget-numbers text-success">
-                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-6 col-xl-3">
-                        <div class="card mb-3 widget-content">
-                            <div class="widget-content-wrapper">
-                                <div class="widget-content-left">
-                                    <div class="widget-heading">Customs</div>
-                                </div>
-                                <div class="widget-content-right">
-                                    <div class="widget-numbers text-success">
-                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download</a>
+                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download <img height="20px" src="{{ asset('icons/word.png')}}" alt=""></a></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
                 <div class="row">
-                    <div class="col-lg-6 col-xl-3">
+
+
+                    <div class="col-lg-6 col-xl-4">
                         <div class="card mb-3 widget-content">
                             <div class="widget-content-wrapper">
                                 <div class="widget-content-left">
-                                    <div class="widget-heading">Visa</div>
+                                    <div class="widget-heading">Partners<a style="color: black;" data-toggle="tooltip" data-placement="top" title="Download something!" class="btn btn-xs">?</a></div>
                                 </div>
                                 <div class="widget-content-right">
                                     <div class="widget-numbers text-success">
-                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download</a>
+                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download <img height="20px" src="{{ asset('icons/word.png')}}" alt=""></a></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-6 col-xl-3">
+                    <div class="col-lg-6 col-xl-4">
                         <div class="card mb-3 widget-content">
                             <div class="widget-content-wrapper">
                                 <div class="widget-content-left">
-                                    <div class="widget-heading">Immigration</div>
+                                    <div class="widget-heading">Customs<a style="color: black;" data-toggle="tooltip" data-placement="top" title="Download something!" class="btn btn-xs">?</a></div>
                                 </div>
                                 <div class="widget-content-right">
                                     <div class="widget-numbers text-success">
-                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download</a>
+                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download <img height="20px" src="{{ asset('icons/word.png')}}" alt=""></a></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-xl-4">
+                        <div class="card mb-3 widget-content">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Visa<a style="color: black;" data-toggle="tooltip" data-placement="top" title="Download something!" class="btn btn-xs">?</a></div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="widget-numbers text-success">
+                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers"> Download <img height="20px" src="{{ asset('icons/word.png')}}" alt=""></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row">
+
+                    <div class="col-lg-6 col-xl-4">
+                        <div class="card mb-3 widget-content">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Immigration<a style="color: black;" data-toggle="tooltip" data-placement="top" title="Download something!" class="btn btn-xs">?</a></div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="widget-numbers text-success">
+                                        <a href="{{ asset('downloads/justapdf.pdf')}}" class="btn btn-primary" download="GuideToUploadUsers">Download <img height="20px" src="{{ asset('icons/word.png')}}" alt=""></a>
                                     </div>
                                 </div>
                             </div>
